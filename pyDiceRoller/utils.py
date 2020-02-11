27e83f9cf6abee09cb2abcafce181f9433d14924
Puma Concolor <puma.9789@gmail.com>
2020-02-11 16:33:47 +0100

    Update utils.py

--- pyDiceRoller/utils.py
+++ pyDiceRoller/utils.py
@@ -1,13 +1,14 @@
 
 welcome_message = 'Welcome to pyDiceRoller, a simple python script to easily \
                     roll dice and roll the ability scores for your next DnD \
-                    character!\n'
-mod_select_message = 'What do you want to roll?\n[0] to roll your ability scores\
+                    character!'
+mod_select_message = 'What do you want to do?\n[0] to roll your ability scores\
                         \n[1] to roll a dice\n[2] to re-roll one or more of your ability scores\
                         \n[3] to exit\n'
 dice_select_message = 'Which die will you roll? d'
 dice_number_message = 'how many dice? '
 score_dice_number_message = 'how many abilities went to shit? '
+farewell_message = 'Good Game!'
 
 def print_scores(score_list):
     result = 'Your six ability scores are:'
